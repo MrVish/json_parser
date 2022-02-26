6f55e3c3ccb8cbb4fe10904544c4a87c96e373b8
MrVish <MrVish@users.noreply.github.com>
2022-02-26 07:50:52 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="MrVish"
+original_name="json_parser"
+original_urlname="json_parser"
+original_description="Awesome json_parser created by MrVish"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv json_parser $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
